dc5f4aa3f7731741937e35750d11a7ffdfa7ab4e
SeanTOSCD <sean@seanchristopherdavis.com>
2024-12-04 08:49:44 -0600

    update Alexs HourLabel output

--- app.R
+++ app.R
@@ -62,7 +62,16 @@ crash_data <- crash_data %>%
     Hour = as.integer(format(DateTime, "%H")),
     
     # Create readable hour labels
-    HourLabel = format(DateTime, "%I %p"),
+    HourLabel = factor(
+      format(DateTime, "%I %p"),
+      levels = c(
+        "12 AM", "01 AM", "02 AM", "03 AM",
+        "04 AM", "05 AM", "06 AM", "07 AM",
+        "08 AM", "09 AM", "10 AM", "11 AM",
+        "12 PM", "01 PM", "02 PM", "03 PM",
+        "04 PM", "05 PM", "06 PM", "07 PM",
+        "08 PM", "09 PM", "10 PM", "11 PM")
+    ),
     
     # Remove extra space from city & state
     CityState = str_trim(CityState),
